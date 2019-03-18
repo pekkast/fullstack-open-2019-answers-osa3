b4e0a4feab9291ea855e2da4e0b70ff5db0deef8
Pekka Turtola <pekka.turtola@yap.fi>
2019-03-18 13:02:08 +0200

    new ui files

---
+++ build/precache-manifest.d99d9b0956f0cd5af35d74b6ad39f33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55df86989543c48121e0",
+    "url": "/static/js/main.d1e40a35.chunk.js"
+  },
+  {
+    "revision": "f9f66f3e4824cb58d023",
+    "url": "/static/js/2.38fa679b.chunk.js"
+  },
+  {
+    "revision": "55df86989543c48121e0",
+    "url": "/static/css/main.8f9390d5.chunk.css"
+  },
+  {
+    "revision": "16ec6bf6752d1498374ee14ed0fb0009",
+    "url": "/index.html"
+  }
+];
